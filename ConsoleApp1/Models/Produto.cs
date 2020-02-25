d6f6d437784955a70474c2b027ca2251a3b15b64
WILSON <shadow7001@outlook.com>
2020-02-25 14:42:45 -0300

    ta uma bosta

--- ConsoleApp1/Models/Produto.cs
+++
@@ -1,9 +0,0 @@
-namespace CargaProdutos.Models
-{
-    public class Produto
-    {
-        public string CodigoBarras { get; set; }
-        public string Nome { get; set; }
-        public double Preco { get; set; }
-    }
-}
